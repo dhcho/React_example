8b29189900f544a7662cee46274449d0ba916a94
dhcho <oop6413@gmail.com>
2021-08-04 21:22:52 +0900

    2021.08.04 react material-ui practice

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Home from './routes/Home';
+import ColorThemeTutorial from './routes/ColorThemeTutorial';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/colorTheme">
+          <ColorThemeTutorial />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
