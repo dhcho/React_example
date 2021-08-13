d134dc116dd7532bc44b3352b26187029eec7e6f
dhcho <oop6413@gmail.com>
2021-08-13 21:09:24 +0900

    2021.08.13 react material ui practice

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Home from './routes/Home';
 import SignIn from './routes/SignIn';
 import SignUp from './routes/SignUp';
+import BoxTutorial from './routes/BoxTutorial';
+import StyleTutorial1 from './routes/StyleTutorial1';
 import ColorThemeTutorial from './routes/ColorThemeTutorial';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
@@ -18,6 +20,12 @@ function App() {
         </Route>
         <Route exact path="/signUp">
           <SignUp />
+        </Route>
+        <Route exact path="/box">
+          <BoxTutorial />
+        </Route>
+        <Route exact path="/style1">
+          <StyleTutorial1 />
         </Route>
         <Route exact path="/colorTheme">
           <ColorThemeTutorial />
